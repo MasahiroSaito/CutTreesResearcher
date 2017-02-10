d83b1b2fd24797f153d91df3b90b2ee6e029eb48
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-10 14:54:09 +0900

    Initial Gradle Project

---
+++ src/main/kotlin/com/masahirosaito/spigot/cuttreesresearcher/CutTreesResearcher.kt
@@ -0,0 +1,14 @@
+package com.masahirosaito.spigot.cuttreesresearcher
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CutTreesResearcher : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
