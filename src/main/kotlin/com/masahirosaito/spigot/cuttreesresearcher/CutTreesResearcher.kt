a167761496291d9202c7561758f0f540361b76c7
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-11 11:38:58 +0900

    CutTreesBreakEventListener 作成

--- src/main/kotlin/com/masahirosaito/spigot/cuttreesresearcher/CutTreesResearcher.kt
+++ src/main/kotlin/com/masahirosaito/spigot/cuttreesresearcher/CutTreesResearcher.kt
@@ -1,11 +1,13 @@
 package com.masahirosaito.spigot.cuttreesresearcher
 
+import com.masahirosaito.spigot.cuttrees.utils.register
+import com.masahirosaito.spigot.cuttreesresearcher.listeners.CutTreesBreakEventListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class CutTreesResearcher : JavaPlugin() {
 
     override fun onEnable() {
-
+        CutTreesBreakEventListener().register(this)
     }
 
     override fun onDisable() {
